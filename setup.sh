e28363a7e99f7f4fedf4b7b51eb48df6877d34ce
wsnell <aussiesnells@gmail.com>
2024-12-04 15:12:32 +1300

    make uv install conditional on it not being installed already

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-curl -LsSf https://astral.sh/uv/install.sh | sh
+echo uv --version
+if [ "$?" -ne "0" ]
+    then
+        curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
 # Add UV to the path
 source $HOME/.local/bin/env
 # Run the script
