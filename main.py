97f26f6e58db00664ac104cfe760772d0622e048
wsnell <aussiesnells@gmail.com>
2024-12-04 15:51:27 +1300

    start building out dataset and model setup

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-import transformer_lens
-print("Hello world")
+from jac import calc_jacobian
+from transformer_lens import HookedTransformer
+from datasets import load_dataset
+
+if __name__ == '__main__':
+    model = HookedTransformer.from_pretrained("gpt2-small")
+    ds = load_dataset("wikimedia/wikipedia", "20231101.en")
